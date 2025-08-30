1a8b9c9f96e203265231e4849744b76a5eef8203
Denis Muniz Silva <denis.m.s.777@hotmail.com>
2025-08-30 17:00:43 -0300

    added rest framework jwt

--- app/settings.py
+++ app/settings.py
@@ -32,12 +32,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'rest_framework',
+    'rest_framework_simplejwt',
 
     'genres',
     'actors',
     'movies',
     'reviews',
+    'authentication',
 ]
 
 MIDDLEWARE = [
@@ -135,3 +138,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+

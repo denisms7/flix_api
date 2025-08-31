cb3241d2208220255d71ab8675110c64976aa8c0
Denis Muniz Silva <denis.m.s.777@hotmail.com>
2025-08-31 11:58:28 -0300

    added comment

--- movies/serializers.py
+++ movies/serializers.py
@@ -9,12 +9,14 @@ class MovieSerializer(serializers.ModelSerializer):
         model = Movie
         fields = "__all__"
 
+    # Delve o rate apenas em modo get get_rate
     def get_rate(self, obj):
         rate = obj.reviews.aggregate(Avg('stars'))['stars__avg']
         if rate:
             return round(rate,1)
         return None
 
+    # validacao de dados  do campo release_date
     def validate_release_date(self, value):
         if value.year < 1888:
             raise serializers.ValidationError('A data de lançamento não pode ser anterior a 1888.')
